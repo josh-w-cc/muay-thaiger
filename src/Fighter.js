e18d23735d8ca20e20e951f9e5b0f3709fbcb0de
josh <j@g.com>
2025-05-31 00:41:09 -0600

    Stats ideas

--- src/Fighter.js
+++ src/Fighter.js
@@ -4,17 +4,48 @@ import {makeAutoObservable} from 'mobx';
 
 class Fighter {
   constructor() {
+    // Innate stats
+    this.durability = 1;
+    this.innateSpeed = 1;
+    this.innateStrength = 1;
+    this.reach = 1;
+    this.vitality = 1;
+    this.willpower = 1;
+
+    // Exnate stats
+    this.constitution = 1;
+    this.skill = 1;
     this.speed = 1;
     this.strength = 1;
-    this.vitality = 1;
+    this.stamina = 1;
 
     makeAutoObservable(this, {}, {autoBind: true});
   }
 
+  // Combat stats (derived)
+  get apm() {
+    return Math.log(this.speed) + Math.sqrt(this.skill);
+  }
+  get attack() {
+    return Math.log(this.stamina) + Math.sqrt(this.speed) + this.skill + this.reach;
+  }
+  get defense() {
+    return Math.log(this.speed) + Math.sqrt(this.stamina) + this.skill;
+  }
+  get health() {
+    return this.stamina + this.constitution + this.durability * this.durability;
+  }
+  get power() {
+    return this.strength + this.speed + this.skill;
+  }
+
+
   train(stat) {
-    const safeStats = ['speed', 'strength', 'vitality'];
-    if(safeStats.includes(stat)) {
-      this[stat]++;
+    const trainingEffect = {
+      constitution: this.vitality, skill: this.willpower, speed: this.innateSpeed, strength: this.innateStrength, stamina: this.vitality,
+    };
+    if(Object.keys(trainingEffect).includes(stat)) {
+      this[stat] += trainingEffect[stat]; //etc
     }
     else {
       console.error('Tried to train unknown stat:', stat);
