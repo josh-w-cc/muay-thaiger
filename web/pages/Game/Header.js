6736d43108e4c8b9778a50d4b93d70ffa3e36d85
josh <hj@g.c>
2026-05-17 17:32:33 -0600

    Facking copilot

--- web/pages/Game/Header.js
+++ web/pages/Game/Header.js
@@ -12,8 +12,8 @@ export default function Header({setScreen}) {
     <div className={css.header}>
       <Button onClick={() => setScreen('fight')}><img alt="Fight" className={css.navImage} src={FightButton} /></Button>
       <Button onClick={() => setScreen('hub')}><img alt="Hub" className={css.navImage} src={HubButton} /></Button>
-      <Button onClick={() => setScreen('shop')}><img alt="Shop" className={css.navImage} src={ShopButton} /></Button>
       <Button onClick={() => setScreen('train')}><img alt="Train" className={css.navImage} src={TrainButton} /></Button>
+      <Button onClick={() => setScreen('shop')}><img alt="Shop" className={css.navImage} src={ShopButton} /></Button>
     </div>
   );
 }
