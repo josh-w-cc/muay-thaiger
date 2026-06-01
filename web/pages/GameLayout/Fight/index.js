6f0824dce7cb047410575373b455fa49dc58bc21
josh-w-cc <corps@grochala.us>
2026-06-01 07:00:55 +0000

    Format fight details with <pre> tag
    Change formatting of fight details to use <pre> for better readability.

--- web/pages/GameLayout/Fight/index.js
+++ web/pages/GameLayout/Fight/index.js
@@ -49,7 +49,7 @@ function formatFightDetails(details) {
   if(details == null) {
     return null;
   }
-  return (<span>{JSON.stringify(details)}</span>);
+  return (<pre>{JSON.stringify(details, null, 2)}</pre>);
 }
 
 function FightMetadata({label, value}) {
